aa8c7198e807b41fc82874420ead1f766bab0368
storywr <storywr@gmail.com>
2021-01-05 01:56:34 -0500

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,22 @@
+events = Event.create([
+  {
+    title: "London Retail Expo",
+    start_datetime: "Mon, 14 Oct 2019",
+    location: "London Excel Centre"
+  },
+  {
+    title: "Enterprise Sales Training Workshop",
+    start_datetime: "Tue, 15 Oct 2019",
+    location: "Expert Sales Company Headquarters"
+  },
+  {
+    title: "Ruby Hack Night",
+    start_datetime: "Fri, 18 Oct 2019",
+    location: "Learnetto Headquarters"
+  },
+  {
+    title: "Beginners Salsa dance meetup",
+    start_datetime: "Sat, 14 Oct 2019",
+    location: "Bar Salsa"
+  }
+])
