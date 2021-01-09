808b528e21df8b1226f95f825cc02d93d43f3b22
storywr <storywr@gmail.com>
2021-01-09 17:33:36 -0500

    edit event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,37 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.order('start_datetime ASC')
+    @events = current_user.events.order('start_datetime ASC')
     render json: @events
   end
 
+  def show
+    @event = Event.find(params[:id])
+    render json: @event.as_json(except: :user_id, include: {user: {only: [:name, :nickname, :image]}})
+                        .merge(currentUserCanEdit: @event.user.email == request.headers['uid'])
+  end
+
   def create
-    @event = Event.new(event_params)
+    @event = current_user.events.new(event_params)
     if @event.save
       render json: @event
+    else
+      render json: @event.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @event = current_user.events.find(params[:id])
+    if @event.update(event_params)
+      render json: @event
+    else
+      render json: @event.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    if @event.destroy
+      head :no_content, status: :ok
     else
       render json: @event.errors, status: :unprocessable_entity
     end
